153f811cf6dc0ad27c7e6c7d8794f77061b16a9b
Dan Schwabe <dfschwabe@yahoo.com>
2016-06-26 06:38:45 -0700

    eliminate consumer loading of HttpListener

--- OnDemandWebServer/MyServer.cs
+++ OnDemandWebServer/MyServer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using Microsoft.Owin.Hosting;
 
 namespace OnDemandWebServer
@@ -6,6 +7,11 @@ namespace OnDemandWebServer
     public class MyServer
     {
         private IDisposable _server;
+
+        static MyServer()
+        {
+            Assembly.Load(typeof (Microsoft.Owin.Host.HttpListener.OwinHttpListener).Assembly.GetName());
+        }
 
         public void Start(string baseUrl)
         {
